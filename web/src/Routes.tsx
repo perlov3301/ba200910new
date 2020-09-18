de03a5f3fb1e05377d405b1fe1a64a23be486435
Alexander Perlov <alexanderperlov@Alexanders-MacBook-Air.local>
2020-09-18 17:15:14 +0300

    insert accessToken into server

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch , Route, Link} from "react-router-dom";
 import { Home } from './pages/Home';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
+import { Bye } from './pages/Bye';
 
 const Routes: React.FC = () => {
    return (
@@ -18,11 +19,15 @@ const Routes: React.FC = () => {
           <div>
           <Link to="/login"><b>login</b></Link>
           </div>
+          <div>
+            <Link to="/bye"><strong><b>Bye</b></strong></Link>
+          </div>
         </header>
        <Switch>
          <Route exact path="/" component={Home}  />
          <Route exact path="/register" component={Register}  />
          <Route exact path="/login" component={Login}  />
+         <Route exact path="/bye" component={Bye}  />
        </Switch>
      </div>
        
---
+++ web/src/pages/Bye.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useByeQuery } from '../generated/graphql';
+
+interface Props {
+
+}
+
+export const Bye: React.FC<Props> = () => {
+    const {data, loading, error} = useByeQuery();
+    if (loading) { return ( <fieldset><legend>Bye page</legend>loadign...</fieldset>); }
+    if (error) { 
+        console.log(error);
+        return ( <fieldset><legend>Bye page</legend>error</fieldset> );
+     }
+     if (!data) { return ( <fieldset><legend>Bye page</legend>no data</fieldset>); }
+        return (
+        <fieldset><legend>Bye page</legend>{data.bye}</fieldset>
+        );
+}
