4a64056e615a576ab1087f07b4e2fade26348492
Alexander Perlov <alexanderperlov@Alexanders-MacBook-Air.local>
2020-09-24 13:54:45 +0300

    non apollo-boost version after advanced   migration for apollo-client 2.5

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import './index.css';
 import { getAccessToken } from './accessToken';
 import { App } from './App';
 
-const client = new ApolloClient({ 
-  uri: "http://localhost:4000/graphql",
-  credentials: "include",
-  request: (operation) => {
-    const accessToken = getAccessToken();
-    if (accessToken) {
-      operation.setContext({
-        headers: {
-          authorization: `bearer ${accessToken}`
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink, Observable } from 'apollo-link';
+
+const cache = new InMemoryCache({});
+
+const requestLink = new ApolloLink((operation, forward) =>
+  new Observable(observer => {
+    let handle: any;
+    Promise.resolve(operation)
+      .then((operation) => {
+        const accessToken = getAccessToken();
+        if (accessToken) {
+          operation.setContext({
+            headers: {
+              authorization: `bearer ${accessToken}`
+            }
+          });
         }
-      });
-    }
-  }
- });
+      }) // access to the operation
+      .then(() => {
+        handle = forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer),
+        });
+      })
+      .catch(observer.error.bind(observer));
+
+    return () => {
+      if (handle) handle.unsubscribe();
+    };
+  })
+);
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+     console.log(graphQLErrors);
+     console.log(networkError);
+    }),
+    requestLink,
+    new HttpLink({
+      uri: "http://localhost:4000/graphql",
+      credentials: 'include'
+    })
+  ]),
+  cache
+});
+
 
 ReactDOM.render( // within ApolloProvider.ts => client:... | DefaultClient<TCache>;
   <ApolloProvider client={client}>
